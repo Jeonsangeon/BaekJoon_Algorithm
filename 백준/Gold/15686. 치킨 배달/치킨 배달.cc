01ee8054871a2c5b1738a2124b119c0a2e1a186e
Sharks_2 <132082492+Jeonsangeon@users.noreply.github.com>
2025-04-05 17:16:09 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2172 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -11,70 +11,70 @@ using namespace std;
 
 int N, M, ret = INF;
 int city[MAX_N][MAX_N];
-
 vector<array<int, 2>> houses;
-vector<array<int, 2>> chickens;
-vector<vector<int>> dist;
+vector<array<int, 2>> chicken;
 vector<bool> chicken_open;
+vector<vector<int>> dist;
 
 void calculateDist() {
     int H = houses.size();
-    int C = chickens.size();
+    int C = chicken.size();
     dist.resize(H, vector<int>(C));
-    for (int i = 0; i < H; ++i) {
-        for (int j = 0; j < C; ++j) {
-            dist[i][j] = abs(houses[i][0] - chickens[j][0]) + abs(houses[i][1] - chickens[j][1]);
+    for(int i = 0; i < H; ++i) {
+        for(int j = 0; j < C; ++j) {
+            dist[i][j] = abs(houses[i][0] - chicken[j][0]) + abs(houses[i][1] - chicken[j][1]);
         }
     }
 }
 
-int calculateCityDistance() {
-    int total = 0;
-    for (int i = 0; i < (int)houses.size(); ++i) {
-        int min_dist = INF;
-        for (int j = 0; j < (int)chickens.size(); ++j) {
-            if (chicken_open[j]) {
-                min_dist = min(min_dist, dist[i][j]);
-            }
-        }
-        total += min_dist;
+int calculateMinDist(int idx) {
+    int min_dist = INF;
+    for(int i = 0; i < (int)chicken.size(); ++i) {
+        if(chicken_open[i])
+            min_dist = min(min_dist, dist[idx][i]);
     }
-    return total;
+    return min_dist;
 }
 
-void dfs(int selected, int idx) {
-    if (selected == M) {
-        ret = min(ret, calculateCityDistance());
+void dfs(int count, int idx) {
+    if(count == M) {
+        int city_dist = 0;
+        for(int i = 0; i < (int)houses.size(); ++i)
+            city_dist += calculateMinDist(i);
+        ret = min(ret, city_dist);
         return;
     }
 
-    for (int i = idx; i < (int)chickens.size(); ++i) {
-        if (!chicken_open[i]) {
+    for(int i = idx; i < (int)chicken.size(); ++i) {
+        if(!chicken_open[i]) {
             chicken_open[i] = true;
-            dfs(selected + 1, i + 1);
+            dfs(count+1, i+1);
             chicken_open[i] = false;
         }
     }
 }
 
-int main() {
+int main(void) {
     ios::sync_with_stdio(0); cin.tie(0);
 
     cin >> N >> M;
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < N; ++j) {
+    for(int i = 0; i < N; ++i) {
+        for(int j = 0; j < N; ++j) {
             cin >> city[i][j];
-            if (city[i][j] == 1)
+            if(city[i][j] == 1) {
                 houses.push_back({i, j});
-            else if (city[i][j] == 2)
-                chickens.push_back({i, j});
+            }
+            else if(city[i][j] == 2) {
+                chicken.push_back({i, j});
+            }
         }
     }
 
-    chicken_open.resize(chickens.size(), false);
+    chicken_open.resize(chicken.size(), false);
     calculateDist();
     dfs(0, 0);
 
-    cout << ret << '\n';
+    cout << ret << "\n";
+
     return 0;
 }
