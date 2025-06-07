dd60cb37fc0fd5aa1f7c455666899fe4287d18d9
Sharks_2 <132082492+Jeonsangeon@users.noreply.github.com>
2025-06-07 20:25:20 +0900

    [level 2] Title: 구명보트, Time: 2.01 ms, Memory: 4.78 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    sort(people.begin(), people.end());
+
+    int left = 0;
+    int right = people.size() - 1;
+    int boats = 0;
+
+    while (left <= right) {
+        if (people[left] + people[right] <= limit) {
+            left++;
+        }
+        right--;
+        boats++;
+    }
+
+    return boats;
+}
